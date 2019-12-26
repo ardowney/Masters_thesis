b57bdd20ecf5f55b6000fff97fefec901c880e0b
ardowney <43677140+ardowney@users.noreply.github.com>
2019-12-26 14:53:11 -0500

    Add files via upload

---
+++ particle_size.R
@@ -0,0 +1,16 @@
+setwd("/Volumes/SAMSUNG_T5/Thesis/data/particle_size/sample_organized")
+size_file <- "particle_size_bins.txt"
+size <- read.delim(size_file, header = FALSE)
+setwd("/Volumes/SAMSUNG_T5/Thesis/data/particle_size/sample_organized/TAL-01")
+files <- list.files()
+bins <- data.frame(size)
+row.names(bins) <- bins[1:92,]
+bins <- bins[1:92,0]
+for (xfile_name in files) {
+  x_data <- read.delim(paste0(xfile_name), header = TRUE)
+  assign(xfile_name, x_data) 
+  bins <- cbind(bins, get(xfile_name))
+}
+
+colnames(bins) <- files
+write.csv(bins, file = "TAL-01.csv")
